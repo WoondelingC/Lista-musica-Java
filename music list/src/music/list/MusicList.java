785e6d4435496c25cf092d431f075d2d22c0ee8b
YormanColina <yorman.colina1997@gmail.com>
2022-02-09 22:28:41 -0500

    prueba de arreglo de musicas

--- music list/src/music/list/MusicList.java
+++ music list/src/music/list/MusicList.java
@@ -10,19 +10,12 @@ public class MusicList {
 
     static Music createMusic;
     
-
+    
     static void listOption(int opt) {
         if(opt == 1) {
             createMusic = new Arraylist("", "", 0, "", "", "");
         }
 
-        createMusic.Title = JOptionPane.showInputDialog("Ingrese el nombre de la canción");
-        createMusic.Date = JOptionPane.showInputDialog("Ingres la fecha de lanzamiento");
-        createMusic.Duration = Integer.parseInt(JOptionPane.showInputDialog("ingrese la duración"));
-        createMusic.Gender = JOptionPane.showInputDialog("ingrese el genero de musica");
-        createMusic.portada = JOptionPane.showInputDialog("ingrese la portada");
-        
-        
         System.out.println(createMusic.getTitle());
         System.out.println("""
                             Su PlayList fue creada con exito
@@ -34,20 +27,13 @@ public class MusicList {
     
     public static void main(String[] args) {
         
-        int option = 1;
-
-        while (option == 1) {
-
-            String music = JOptionPane.showInputDialog("""
-                                                      Crear una nave: 
-                                                       1- crear una playList
-                                                       2- Salir""");
-            option = Integer.parseInt(music);
-
-            if (option == 1) {
-                listOption(option);
+        Music musicas[] = new Arraylist[3];
+        String frutas[] = {"Uva", "Melon", "Mango"};
+        
+        for (int i = 0; i < frutas.length; i++) {
+            System.out.println("frutas = " + frutas[i]);    
             }
-
+            //System.out.println("frutas = " + frutas[i]);
         }
     }
-}
+
