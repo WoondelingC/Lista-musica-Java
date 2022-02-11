535931a9768f9ddb29840085ad2d2ebb36870001
JBritoAvilez <jonathanbrito532@outlook.com>
2022-02-11 01:37:43 -0500

    Update FilterMusic.java
    Agregando los metodos de filtrar por año y ordenar por año y duracion

--- music list/src/music/list/FilterMusic.java
+++ music list/src/music/list/FilterMusic.java
@@ -24,8 +24,42 @@ public class FilterMusic implements IMusic {
     }
 
     @Override
-    public List<Album> FilterYear(Album[] lista) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public List<Album> FilterYear(Album[] lista,int Date) {
     
+           List<Album> ot = new ArrayList();
+        
+        for(int i = 0; i < lista.length; i++){
+        
+            if (lista[i].Date == Date) {
+                ot.add(lista[i]);
+            }
+        }
+        
+        return ot; 
+    }    
+
+    @Override
+    public List<Album> FilterYearDuration(Album[] lista) {
+        List<Album> ot = new ArrayList();
+        
+        for(int i = 0; i<(lista.length-1);i++){
+            for(int j = 0;j<(lista.length-1);i++){
+                if((lista[j].Date > lista[j+1].Date) &&(lista[j].Duration > lista[j+1].Duration)){
+                    Album aux = lista[j];
+                    lista[j] = lista[j+1];
+                    lista[j+1] = aux;
+//                    for(int k = 0; k<lista.length;k++){
+//                        System.out.println(lista[k].Title);
+//                    }
+                }
+            }
+        }
+        
+//        for(int i = 0; i<lista.length; i++){
+//            if((lista[i].Date == Date) && (lista[i].Duration == Duration)){
+//                ot.add(lista[i]);
+//            }
+//        }
+        return ot;
+    }
 }
