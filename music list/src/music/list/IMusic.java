24d219c69bbd8267e2473b9eb43a697f1aaca577
JBritoAvilez <jonathanbrito532@outlook.com>
2022-02-11 23:50:09 -0500

    Update IMusic.java
    Se documentan los metodos de la clase FiltroMusica

--- music list/src/music/list/IMusic.java
+++ music list/src/music/list/IMusic.java
@@ -11,11 +11,28 @@ import java.util.List;
  * @author Yorman Colina
  */
 public interface IMusic {
-    
-    public List<Album> FilterGenre(Album[] lista, String genero);
-    
-    public List<Album> FilterYear(Album[] lista,int Date);
-    
-    public List<Album> FilterYearDuration(Album[] lista);
+    /**
+     * Creamos el metodo Arreglo FiltrarGenero en el cual ponemos como parametros
+     * Genero y el arreglo Album Lista para asi ser sobrescrito en las clases que necesiten el metodo
+     * @param list
+     * @param Gender
+     * @return 
+     */
+    public List<Album> FilterGenre(Album[] list, String Gender);
+    /**
+     * Creamos el metodo Arreglo FiltrarAño en el cual ponemos como parametros
+     * Fecha y y el arreglo Album Lista para asi ser sobrescrito en las clases que necesiten el metodo
+     * @param list
+     * @param Date
+     * @return 
+     */
+    public List<Album> FilterYear(Album[] list,int Date);
+    /**
+     * Creamos el metodo Arreglo FiltrarAñoDuracion en el cual ponemos como parametros
+     * el arreglo Album Lista para asi ser sobrescrito en las clases que necesiten el metodo
+     * @param list
+     * @return 
+     */
+    public List<Album> FilterYearDuration(Album[] list);
     
 }
